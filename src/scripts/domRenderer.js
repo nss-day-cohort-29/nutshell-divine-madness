0500844bb83c002ac65e39f5639613ac3fcf8cf0
jdbbaugh <jbaugh78@msn.com>
2019-01-10 15:51:30 -0600

    more stuff

--- src/scripts/domRenderer.js
+++ src/scripts/domRenderer.js
@@ -3,6 +3,59 @@ import nomadData from "./nomadData";
 // import messages from "./messages";
 import friends from "./friends";
 import eventListeners from "./eventListeners";
+import news from "./news"
+console.log("I'm working")
+
+// template for object to pass into nomadData.connectToData() if you are doing a GET
+
+// let fetchTest = {
+
+//     "dataSet" : "users",
+//     "fetchType" : "GET",
+//     "dataBaseObject" : "",
+//     "embedItem" : "?_embed=events"
+// }
+
+// template for object to pass into nomadData.connectToData() if you are doing a POST
+
+// let fetchTest2 = {
+
+//     "dataSet" : "events",
+//     "fetchType" : "POST",
+//     "dataBaseObject" : {
+//       "userId": 1,
+//       "eventName": "yet another toga party",
+//       "eventDate": "2-12",
+//       "eventTime": "3:00pm",
+//       "eventLocation": "Vegas"
+//     }
+// }
+
+// template for object to pass into nomadData.connectToData() if you are doing a PUT
+
+// let fetchTest3 = {
+
+//     "putId" : 2,
+//     "dataSet" : "events",
+//     "fetchType" : "PUT",
+//     "dataBaseObject" : {
+//       "id" : 2,
+//       "userId": 1,
+//       "eventName": "another toga party",
+//       "eventDate": "2-15",
+//       "eventTime": "3:00pm",
+//       "eventLocation": "Vegas"
+//     }
+// }
+
+// nomadData.connectToData(fetchTest3)
+//NEWS SECTION
+// news.save();
+// news.allSaved();
+// news.getNews();
+// news.newsElementCreator();
+
+import messages from "./messages";
 
 
 // messages.createMessageBoard();
