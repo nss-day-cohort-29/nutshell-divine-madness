668d962d88c95b3aa240bad2543acb17f76a0d9d
colebryant <cobryant47@gmail.com>
2019-01-14 15:48:58 -0600

    working commit

--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -158,62 +158,6 @@ const eventListeners = {
     /*========================================================================================================
     END OF NAVIGATION EVENTLISTENERS
     =========================================================================================================*/
-
-    friendsDeleteFriend () {
-        console.log(event.target);
-
-    },
-    handleEventSaveButton() {
-        const nameInputted = document.querySelector("#eventName").value;
-        const dateInputted = document.querySelector("#eventDate").value;
-        const timeInputted = document.querySelector("#eventTime").value;
-        const locationInputted = document.querySelector("#eventLocation").value;
-
-        const eventObject = {
-            eventName: nameInputted,
-            eventDate: dateInputted,
-            eventTime: timeInputted,
-            eventLocation: locationInputted
-        };
-
-
-        nomadData.connectToData({
-            dataSet: "events",
-            fetchType: "POST",
-            dataBaseObject: {
-                userId: sessionStorage.getItem("userId"),
-                eventName: eventObject.eventName,
-                eventDate: eventObject.eventDate,
-                eventTime: eventObject.eventTime,
-                eventLocation: eventObject.eventLocation
-            }
-        })
-        .then( () => {
-            events.appendUserEvents();
-        });
-      },
-      handleEventDeleteButton() {
-        const idToDelete = event.target.id.split("--")[1];
-        nomadData.connectToData({
-            deleteId: idToDelete,
-            dataSet: "events",
-            fetchType: "DELETE",
-            dataBaseObject: {
-                userId: sessionStorage.getItem("userId")
-            }
-        })
-        .then( () => {
-            events.appendUserEvents();
-        });
-      },
-      handleEventEditButton() {
-        const idToEdit = event.target.id.split("--")[1];
-        // const eventObject =
-        domComponents.createEventEditInput(idToEdit, )
-    },
-    handleEventUpdateButton() {
-
-    }
 };
 
 export default eventListeners;
